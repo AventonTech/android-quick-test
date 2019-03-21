e294fdf02c4c36f402f783ee0094c858b2853b4d
Alvaro-Personal <alexche7717@gmail.com>
2019-03-21 01:50:23 -0600

    GeoJson built from server

--- app/src/main/java/ni/alvaro/dev/aventontest/views/MainActivity.java
+++ app/src/main/java/ni/alvaro/dev/aventontest/views/MainActivity.java
@@ -1,30 +1,58 @@
 package ni.alvaro.dev.aventontest.views;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.gson.Gson;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.geojson.Geometry;
+import com.mapbox.geojson.LineString;
+import com.mapbox.geojson.Point;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.MapboxMapOptions;
+import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.maps.SupportMapFragment;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.app.Person;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import ni.alvaro.dev.aventontest.R;
+import ni.alvaro.dev.aventontest.networking.RetrofitHelper;
+import ni.alvaro.dev.aventontest.utils.MapMarker;
+import ni.alvaro.dev.aventontest.utils.ServerResponse;
 import ni.alvaro.dev.aventontest.utils.Util;
-import ni.alvaro.dev.aventontest.views.fragments.MapFragment;
 import ni.alvaro.dev.aventontest.views.fragments.PermissionsDeniedFragment;
 import ni.alvaro.dev.aventontest.views.fragments.ProfileFragment;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity implements PermissionsDeniedFragment.OnFragmentInteractionListener,
-        MapFragment.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity implements PermissionsDeniedFragment.OnFragmentInteractionListener,MapboxMap.OnMapClickListener{
 
-    final Fragment mapsFragment = MapFragment.newInstance();
+    private static final String MAP_TAG = "map_tag";
+    private static final String MARKERS = "MARKERS";
+    MapboxMapOptions options = new MapboxMapOptions().camera(new CameraPosition.Builder()
+            .zoom(12)
+            .build());
+    final SupportMapFragment mapsFragment = SupportMapFragment.newInstance(options);
     final Fragment profileFragment = ProfileFragment.newInstance();
     final Fragment permissionDeniedFragment = PermissionsDeniedFragment.newInstance(R.string.location_request_rationale);
     private Fragment currentFrag = mapsFragment;
@@ -60,18 +88,87 @@ public class MainActivity extends AppCompatActivity implements PermissionsDenied
         return false;
     };
     private String TAG = MainActivity.class.getSimpleName();
+    private MapboxMap mapBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fm.beginTransaction().add(R.id.main_content, mapsFragment, MapFragment.TAG).commit();
+        fm.beginTransaction().add(R.id.main_content, mapsFragment, MAP_TAG).commit();
         fm.beginTransaction().add(R.id.main_content, profileFragment, ProfileFragment.TAG).hide(profileFragment).commit();
         fm.beginTransaction().add(R.id.main_content, permissionDeniedFragment, PermissionsDeniedFragment.TAG).hide(permissionDeniedFragment).commit();
         BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         navigation.setSelectedItemId(R.id.maps);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED){
+            loadMap();
+
+        }
+    }
+
+
+    @SuppressLint("MissingPermission")
+    private void findUserLocation(Style style) {
+        // Get an instance of the component
+        LocationComponent locationComponent = mapBox.getLocationComponent();
+
+        locationComponent.activateLocationComponent(MainActivity.this, style);
+
+        locationComponent.setLocationComponentEnabled(true);
+
+        locationComponent.setCameraMode(CameraMode.TRACKING);
+        locationComponent.setRenderMode(RenderMode.COMPASS);
+        RetrofitHelper.getInstance().getUserService().getMarkers().enqueue(new Callback<ServerResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<ServerResponse> call, @NonNull Response<ServerResponse> response) {
+                if (response.isSuccessful()){
+                    ServerResponse s = response.body();
+                    if (s != null) {
+                        Gson g = new Gson();
+                        if (s.getError() == 0){
+                            List<MapMarker> markers = s.getResult();
+                            ArrayList<Feature> features = new ArrayList<>();
+
+                            for (MapMarker m :
+                                    markers) {
+                                Feature f = Feature.fromGeometry(Point.fromLngLat(Double.parseDouble(m.getLg()), Double.parseDouble(m.getLt())));
+                                f.addStringProperty("name",m.getName());
+                                features.add(f);
+                            }
+
+                            FeatureCollection featureCollection = FeatureCollection.fromFeatures(features);
+
+
+                            GeoJsonSource geoJsonSource = new GeoJsonSource("users-geojson-source", featureCollection);
+                            style.addSource(geoJsonSource);
+
+
+                        }else{
+                            Log.e(TAG, "onResponse: Server responded with error code::"+s.getErrorCode()+" --> "+s.getMsg());
+                        }
+                    }
+                }else{
+                    Log.e(TAG, "onResponse: No data from server retrieved");
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<ServerResponse> call, @NonNull Throwable t) {
+                Log.i(TAG, "onFailure: No data from server retrieved!");
+            }
+        });
+
+    }
+
+    private void loadMap() {
+        mapsFragment.getMapAsync(mapboxMap -> mapboxMap.setStyle(Style.MAPBOX_STREETS, style -> {
+            // Map is set up and the style has loaded. Now you can add data or make other map adjustments
+            this.mapBox = mapboxMap;
+            findUserLocation(style);
+        }));
     }
 
 
@@ -86,6 +183,8 @@ public class MainActivity extends AppCompatActivity implements PermissionsDenied
                     fm.beginTransaction().hide(currentFrag).show(mapsFragment)
                             .commit();
                     currentFrag = mapsFragment;
+
+                   loadMap();
 
                 }
             }
@@ -127,8 +226,45 @@ public class MainActivity extends AppCompatActivity implements PermissionsDenied
         }
     }
 
+
     @Override
-    public void onPersonMarkerInteraction(Person person) {
-        Log.i(TAG, "onPersonMarkerInteraction: Person clicked " + person.getName());
+    public void onResume() {
+        super.onResume();
+        mapsFragment.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapsFragment.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapsFragment.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapsFragment.onLowMemory();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapsFragment.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapsFragment.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public boolean onMapClick(@NonNull LatLng point) {
+        return false;
     }
 }

7fa29eca086b3ba890527d50d66b49f6a7791bff
Alvaro-Personal <alexche7717@gmail.com>
2019-03-21 04:33:30 -0600

    Bintray is down, slowing down process

--- app/src/main/java/ni/alvaro/dev/aventontest/views/fragments/ProfileFragment.java
+++ app/src/main/java/ni/alvaro/dev/aventontest/views/fragments/ProfileFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -12,6 +15,7 @@ import ni.alvaro.dev.aventontest.R;
 
 public class ProfileFragment extends Fragment {
     public static final String TAG = ProfileFragment.class.getSimpleName();
+
     public ProfileFragment() {
 
     }
@@ -24,7 +28,7 @@ public class ProfileFragment extends Fragment {
      */
 
     public static ProfileFragment newInstance() {
-       return new ProfileFragment();
+        return new ProfileFragment();
 
     }
 
@@ -33,7 +37,11 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+        Glide.with(this)
+                .load("https://aventontech.com/images/footer-aventon-logo.jpg")
+                .into(((ImageView) v.findViewById(R.id.profile_image)));
+        return v;
     }
 
 
